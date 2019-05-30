3ec56a921b17fed6ece4dd95e2ec1beca023a875
Zach Pardee <zachpardee@gmail.com>
2019-05-30 17:01:05 -0600

    added actual github and linked in page

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,11 +11,11 @@ Project.destroy_all
 Technology.destroy_all
 User.destroy_all
 
-zach = User.create(first: "Zach", last: "Pardee", github: "www.github.com", linkedin:"www.linkedin.com")
+zach = User.create(first: "Zach", last: "Pardee", github: "www.github.com/zachpardee/", linkedin:"www.linkedin.com/in/williamzpardee")
 User.create(first: "Johnny", last: "AppleSeed", github: "www.github.com", linkedin:"www.linkedin.com")
 User.create(first: "John", last: "Elway", github: "www.github.com", linkedin:"www.linkedin.com")
 
-Project.create(title:"Project Title" , description:"This is a project Description", image:"someimagelink.com", githublink:"github.com", user_id: zach.id)
+Project.create(title:"Project Title" , description:"This is a project Description", image:"http://www.illuminationworksllc.com/wp-content/uploads/2017/04/ProjectManagement-1.jpg", githublink:"github.com", user_id: zach.id)
 
 Technology.create(name: "Ruby", icon: "https://cdn.iconscout.com/icon/free/png-256/ruby-46-1175101.png", user_id: zach.id)
 
