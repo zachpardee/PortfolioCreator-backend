bfca09deb8ce3075baba7b9010161d617f2152f4
Zach Pardee <zachpardee@gmail.com>
2019-05-30 11:45:35 -0600

    added post to projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,4 +3,19 @@ class ProjectsController < ApplicationController
         @projects = Project.all
         render json: @projects
     end
+
+    # def create
+    #     @project = Project.create(params[:project])
+    # end
+
+    def create
+        @project = Project.create(project_params)
+    end
+
+
+    private
+
+    def project_params
+        params.require(:project).permit(:title, :description, :image, :githublink, :user_id)
+    end
 end
